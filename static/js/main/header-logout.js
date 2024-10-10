90c2f93ca75d1208a466ac20ba1cf2a633933d1c
000jisoo <000jisoo@gmail.com>
2024-10-10 18:37:27 +0900

    main-header

--- static/js/main/header-logout.js
+++ static/js/main/header-logout.js
@@ -1,20 +1,21 @@
 const subNavigations = document.querySelectorAll(".sub-navigation");
 
-// 각 메뉴 아이템에 대해 이벤트 리스너를 추가
 subNavigations.forEach((sub) => {
-    sub.addEventListener("mouseenter", (e) => {
-        // 마우스가 올라갔을 때 다른 메뉴들 색상이 변경 된다
-        subNavigations.forEach((sub) => {
-            if (sub !== font) {
-                menu.classList.add("main_heder_menu_bar__zwZYy");
-            }
-        });
+    sub.addEventListener("mouseover", (e) => {
+        e.target.style.color = "#002fff";
     });
 
-    // 마우스가 떠났을 때 색상
-    font.addEventListener("mouseleave", (e) => {
-        subNavigations.forEach((sub) => {
-            sub.classList.remove("main_heder_menu_bar__zwZYy"); // 글씨 색 변경 클래스 제거
-        });
+    sub.addEventListener("mouseout", (e) => {
+        e.target.style.color = "#212121";
     });
+});
+
+const searchBox = document.querySelector(".search-box");
+
+searchBox.addEventListener("mouseover", (e) => {
+    e.target.style.backgroundColor = "#fafafa;";
+});
+
+searchBox.addEventListener("mouseout", (e) => {
+    e.target.style.backgroundColor = "none";
 });
