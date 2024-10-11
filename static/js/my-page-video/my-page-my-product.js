48cb6fbe33eab10adc3b0bb662a57e4cc98f398f
guswnsdldl <guswnsdldl@naver.com>
2024-10-11 12:38:48 +0900

    ohj

--- static/js/my-page-video/my-page-my-product.js
+++ static/js/my-page-video/my-page-my-product.js
@@ -19,17 +19,48 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 document.addEventListener("DOMContentLoaded", function () {
-    const btnPublic = document.querySelector(".btn-choice.btn-public");
-    const btnSecret = document.querySelector(".btn-choice.btn-secret");
+    const choiceGroups = document.querySelectorAll(".btn-group.choice-group");
 
-    // 클릭 이벤트 설정
-    function toggleActiveClass(event) {
-        // 두 버튼에 active 클래스를 교환
-        btnPublic.classList.toggle("active");
-        btnSecret.classList.toggle("active");
-    }
+    choiceGroups.forEach(function (group) {
+        const btnPublic = group.querySelector(".btn-choice.btn-public");
+        const btnSecret = group.querySelector(".btn-choice.btn-secret");
 
-    // 두 버튼에 이벤트 리스너 추가
-    btnPublic.addEventListener("click", toggleActiveClass);
-    btnSecret.addEventListener("click", toggleActiveClass);
+        // 클릭 이벤트 설정
+        function toggleActiveClass(event) {
+            // 두 버튼에 active 클래스를 교환
+            btnPublic.classList.toggle("active");
+            btnSecret.classList.toggle("active");
+        }
+
+        // 각 버튼에 이벤트 리스너 추가
+        btnPublic.addEventListener("click", toggleActiveClass);
+        btnSecret.addEventListener("click", toggleActiveClass);
+    });
+});
+document.addEventListener("DOMContentLoaded", function () {
+    // "구매한 사람들" 버튼을 선택합니다.
+    const toggleButtons = document.querySelectorAll(
+        '.btn-icon-edit-scrap[name="toggle_btn"]'
+    );
+
+    // 모든 setting-table을 숨깁니다.
+    const settingTables = document.querySelectorAll(".setting-table");
+    settingTables.forEach(function (table) {
+        table.style.display = "none"; // 처음엔 숨김
+    });
+
+    // 버튼 클릭 시 설정 테이블을 표시/숨김 처리하는 이벤트 리스너
+    toggleButtons.forEach(function (button, index) {
+        button.addEventListener("click", function () {
+            const settingTable = settingTables[index];
+            if (
+                settingTable.style.display === "none" ||
+                settingTable.style.display === ""
+            ) {
+                settingTable.style.display = "block"; // 보여줍니다.
+            } else {
+                settingTable.style.display = "none"; // 숨깁니다.
+            }
+        });
+    });
 });
