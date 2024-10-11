4c97de86210ca6a521444558ee039caa213d9277
guswnsdldl <guswnsdldl@naver.com>
2024-10-11 11:27:43 +0900

    ohj

--- static/js/my-page-video/my-page-my-product.js
+++ static/js/my-page-video/my-page-my-product.js
@@ -18,3 +18,18 @@ document.addEventListener("DOMContentLoaded", function () {
         iconFold.classList.toggle("fold-on");
     });
 });
+document.addEventListener("DOMContentLoaded", function () {
+    const btnPublic = document.querySelector(".btn-choice.btn-public");
+    const btnSecret = document.querySelector(".btn-choice.btn-secret");
+
+    // 클릭 이벤트 설정
+    function toggleActiveClass(event) {
+        // 두 버튼에 active 클래스를 교환
+        btnPublic.classList.toggle("active");
+        btnSecret.classList.toggle("active");
+    }
+
+    // 두 버튼에 이벤트 리스너 추가
+    btnPublic.addEventListener("click", toggleActiveClass);
+    btnSecret.addEventListener("click", toggleActiveClass);
+});
