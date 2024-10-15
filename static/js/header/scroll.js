c050087a58a78588ad6c4f45e27687c4c1f50dfe
000jisoo <000jisoo@gmail.com>
2024-10-15 14:06:28 +0900

    header

---
+++ static/js/header/scroll.js
@@ -0,0 +1,7 @@
+const body = document.getElementsByTagName("body");
+const headerNav = document.querySelector(".header-nav");
+
+// window.addEventListener("scroll", () => {
+//     console.log("스크롤");
+//     headerNav.classList.add("sticky");
+// });
