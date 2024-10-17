74114bf74553c5f0de95ea15b22a0d439058df48
000jisoo <000jisoo@gmail.com>
2024-10-17 17:20:00 +0900

    admin

--- static/js/admin/test-modal.js
+++ static/js/admin/test-modal.js
@@ -1,23 +1,27 @@
 // **모달창 기능 구현
-const editBtn = document.getElementById("edit-btn");
-const editModal = document.getElementById("edit-modal");
+const editBtns = document.querySelectorAll("#edit-btn");
+const editFullModal = document.getElementById("edit-full-modal");
 const closeButton = document.getElementById("close-button");
-const backgroundOverlay = document.getElementById("background-overlay");
+const backgroundOverlay = document.querySelector(
+    "#edit-full-modal .background-overlay"
+);
 
 // 수정 버튼 클릭했을 때
-editBtn.addEventListener("click", () => {
-    // 모달창 나옴.
-    editModal.style.display = "block";
+editBtns.forEach((editBtn) => {
+    editBtn.addEventListener("click", () => {
+        // 모달창 나옴.
+        editFullModal.style.display = "block";
+    });
 });
 
 // 닫기 버튼 클릭했을 때
 closeButton.addEventListener("click", () => {
     // 모달창 사라짐.
-    editModal.style.display = "none";
+    editFullModal.style.display = "none";
 });
 
 // 백그라운드 클릭했을 때
 backgroundOverlay.addEventListener("click", () => {
     // 모달창 사라짐.
-    editModal.style.display = "none";
+    editFullModal.style.display = "none";
 });
