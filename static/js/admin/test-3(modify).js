d117cd3fd4c78b501aeef4442927f9ea1e4d0079
000jisoo <000jisoo@gmail.com>
2024-10-20 23:06:47 +0900

    admin

--- static/js/admin/test-3(modify).js
+++ static/js/admin/test-3(modify).js
@@ -23,8 +23,34 @@ function setupCheckboxes(sectionId) {
         });
     });
 }
+setupCheckboxes("#member-section");
+setupCheckboxes("#notice-section");
+setupCheckboxes("#inquiry-section");
+setupCheckboxes("#payment-section");
 
-// 상태 수정 모달창을 수행하는 함수
+// ####공통 로직: 필터링 기능을 수행하는 함수
+function setupFilterOptions(sectionId) {
+    const sortFilterOptionsVideo = document.querySelectorAll(
+        `${sectionId} .sort-filter-option`
+    );
+
+    // 필터링(눌렀을 때)
+    sortFilterOptionsVideo.forEach((option) => {
+        option.addEventListener("click", () => {
+            // selected 클래스 추가/제거
+            document
+                .querySelector(`${sectionId} .sort-filter-option.selected`)
+                .classList.remove("selected");
+            option.classList.add("selected");
+        });
+    });
+}
+setupFilterOptions("#member-section");
+setupFilterOptions("#notice-section");
+setupFilterOptions("#inquiry-section");
+setupFilterOptions("#payment-section");
+
+// 회원 상태 모달창을 수행하는 함수
 function showStatusEditModal(sectionId) {
     const editbtns = document.querySelectorAll(`${sectionId} .edit-btn`);
     const modal = document.querySelector(`${sectionId} .status-edit-modal`);
@@ -34,7 +60,7 @@ function showStatusEditModal(sectionId) {
     const backgroundOverlay = document.querySelector(
         `${sectionId} .status-edit-modal .background-overlay`
     );
-    // 신고 버튼 클릭했을 때
+    // 버튼 클릭했을 때
     editbtns.forEach((btns) => {
         btns.addEventListener("click", () => {
             // 모달창 나옴.
@@ -46,7 +72,7 @@ function showStatusEditModal(sectionId) {
         // 모달창 사라짐.
         modal.style.display = "none";
     });
-    // 신고 처리 선택 및 저장
+    // 선택 및 저장
     const choiceBtns = document.querySelectorAll(
         `${sectionId} .status-edit-modal .choice-container > input[type=button]`
     );
@@ -81,26 +107,36 @@ function showStatusEditModal(sectionId) {
 }
 showStatusEditModal("#member-section");
 
-// ####공통 로직: 필터링 기능을 수행하는 함수
-function setupFilterOptions(sectionId) {
-    const sortFilterOptionsVideo = document.querySelectorAll(
-        `${sectionId} .sort-filter-option`
+// **공지사항 작성/수정 모달창**
+function showNoticeModal(type) {
+    const modal = document.querySelector(`.notice-full-modal.${type}`);
+    const openBtns = document.querySelectorAll(
+        type === "write"
+            ? ".notice-write.selected-btn"
+            : ".edit-btn.notice-edit"
     );
+    const closeBtn = modal.querySelector(".close-button");
+    const backgroundOverlay = modal.querySelector(".background-overlay");
 
-    // 필터링(눌렀을 때)
-    sortFilterOptionsVideo.forEach((option) => {
-        option.addEventListener("click", () => {
-            // selected 클래스 추가/제거
-            document
-                .querySelector(`${sectionId} .sort-filter-option.selected`)
-                .classList.remove("selected");
-            option.classList.add("selected");
+    // 열기 버튼 클릭 시 모달창 표시
+    openBtns.forEach((btn) => {
+        btn.addEventListener("click", () => {
+            modal.style.display = "flex";
         });
     });
-}
 
-// ####각 섹션에 적용####
-setupFilterOptions("#member-section");
+    // 닫기 버튼 클릭 시 모달창 닫기
+    closeBtn.addEventListener("click", () => {
+        modal.style.display = "none";
+    });
+
+    // 배경 클릭 시 모달창 닫기
+    backgroundOverlay.addEventListener("click", () => {
+        modal.style.display = "none";
+    });
+}
+showNoticeModal("write"); // 공지사항 작성 모달
+showNoticeModal("edit"); // 공지사항 수정 모달
 
 // ####공통 로직: '영상' 또는 '글' 클릭 시 처리####
 function toggleContent(sectionId) {
@@ -502,3 +538,60 @@ function showAnsweredModal(sectionId, type) {
 // **함수 호출**
 showAnsweredModal("#inquiry-section", "unanswered");
 showAnsweredModal("#inquiry-section", "completed");
+
+// 환불 상태 모달창을 수행하는 함수
+function showStatusRefund(sectionId) {
+    const editbtns = document.querySelectorAll(`${sectionId} .edit-btn`);
+    const modal = document.querySelector(`${sectionId} .refund-status-modal`);
+    const btnComplete = document.querySelector(
+        `${sectionId} .refund-status-modal .btn-complete`
+    );
+    const backgroundOverlay = document.querySelector(
+        `${sectionId} .refund-status-modal .background-overlay`
+    );
+    // 버튼 클릭했을 때
+    editbtns.forEach((btns) => {
+        btns.addEventListener("click", () => {
+            // 모달창 나옴.
+            modal.style.display = "flex";
+        });
+    });
+    // 백그라운드 클릭했을 때
+    backgroundOverlay.addEventListener("click", () => {
+        // 모달창 사라짐.
+        modal.style.display = "none";
+    });
+    // 선택 및 저장
+    const choiceBtns = document.querySelectorAll(
+        `${sectionId} .refund-status-modal .choice-container > input[type=button]`
+    );
+    choiceBtns.forEach((choiceBtn, i) => {
+        choiceBtn.addEventListener("click", (e) => {
+            const clickedIndex = i;
+            choiceBtns.forEach((choice, index) => {
+                if (index !== clickedIndex) {
+                    choice.classList.remove("on");
+                }
+            });
+            choiceBtns[clickedIndex].classList.toggle("on");
+            console.log(choiceBtns[clickedIndex].classList.contains("on"));
+        });
+    });
+    btnComplete.addEventListener("click", () => {
+        let anyChoiseSelected = false;
+        // choiceBtns "on" 클래스를 가진 항목이 있는지 확인
+        choiceBtns.forEach((choice) => {
+            if (choice.classList.contains("on")) {
+                anyChoiseSelected = true;
+            }
+        });
+        // 선택된 항목이 있으면 모달창이 닫힌다.
+        if (anyChoiseSelected) {
+            alert("변경합니다.");
+            modal.style.display = "none";
+        } else {
+            alert("선택해주세요.");
+        }
+    });
+}
+showStatusRefund("#payment-section");
